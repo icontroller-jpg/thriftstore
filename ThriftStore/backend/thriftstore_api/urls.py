abc48c3b606049e9e41894f98c98e159c64c3a55
icontroller-jpg <walternyika07@gmail.com>
2026-03-10 22:09:39 +0530

    run

--- ThriftStore/backend/thriftstore_api/urls.py
+++ ThriftStore/backend/thriftstore_api/urls.py
@@ -2,12 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from ..users.views import signup
 
 urlpatterns = [
 
     path("admin/", admin.site.urls),
 
     path("api/products/", include("products.urls")),
+    path("api/signup/", signup),
+    path("api/login/", TokenObtainPairView.as_view()),
 
 ]
 
